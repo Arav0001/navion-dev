9c6b60e31d71146e1caccde23f137f8b75cc3323
ASTR0N0M3R11 <arav.sonawane@gmail.com>
2024-10-28 22:20:43 -0400

    Initial commit

---
+++ Code/CNTRL/Core/Inc/main.h
@@ -0,0 +1,125 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define GENERAL_LED_Pin GPIO_PIN_13
+#define GENERAL_LED_GPIO_Port GPIOC
+#define PYRO_1_Pin GPIO_PIN_0
+#define PYRO_1_GPIO_Port GPIOC
+#define PYRO_1_SENSE_Pin GPIO_PIN_1
+#define PYRO_1_SENSE_GPIO_Port GPIOC
+#define PYRO_2_Pin GPIO_PIN_2
+#define PYRO_2_GPIO_Port GPIOC
+#define PYRO_2_SENSE_Pin GPIO_PIN_3
+#define PYRO_2_SENSE_GPIO_Port GPIOC
+#define VBAT_SENSING_Pin GPIO_PIN_0
+#define VBAT_SENSING_GPIO_Port GPIOA
+#define BUZZER_Pin GPIO_PIN_1
+#define BUZZER_GPIO_Port GPIOA
+#define SPI1_CSB_Pin GPIO_PIN_4
+#define SPI1_CSB_GPIO_Port GPIOA
+#define SD_DETECT_Pin GPIO_PIN_4
+#define SD_DETECT_GPIO_Port GPIOC
+#define CODE_1_Pin GPIO_PIN_5
+#define CODE_1_GPIO_Port GPIOC
+#define CODE_2_Pin GPIO_PIN_0
+#define CODE_2_GPIO_Port GPIOB
+#define CODE_3_Pin GPIO_PIN_1
+#define CODE_3_GPIO_Port GPIOB
+#define STATUS_R_Pin GPIO_PIN_2
+#define STATUS_R_GPIO_Port GPIOB
+#define STATUS_G_Pin GPIO_PIN_10
+#define STATUS_G_GPIO_Port GPIOB
+#define STATUS_B_Pin GPIO_PIN_11
+#define STATUS_B_GPIO_Port GPIOB
+#define SPI2_CSB_Pin GPIO_PIN_12
+#define SPI2_CSB_GPIO_Port GPIOB
+#define FIN_SERVO_1_Pin GPIO_PIN_6
+#define FIN_SERVO_1_GPIO_Port GPIOC
+#define FIN_SERVO_2_Pin GPIO_PIN_7
+#define FIN_SERVO_2_GPIO_Port GPIOC
+#define FIN_SERVO_3_Pin GPIO_PIN_8
+#define FIN_SERVO_3_GPIO_Port GPIOC
+#define FIN_SERVO_4_Pin GPIO_PIN_9
+#define FIN_SERVO_4_GPIO_Port GPIOC
+#define TVC_SERVO_X_Pin GPIO_PIN_8
+#define TVC_SERVO_X_GPIO_Port GPIOA
+#define TVC_SERVO_Y_Pin GPIO_PIN_10
+#define TVC_SERVO_Y_GPIO_Port GPIOA
+#define SPI3_CSB_Pin GPIO_PIN_15
+#define SPI3_CSB_GPIO_Port GPIOA
+#define PYRO_3_Pin GPIO_PIN_2
+#define PYRO_3_GPIO_Port GPIOD
+#define PYRO_3_SENSE_Pin GPIO_PIN_3
+#define PYRO_3_SENSE_GPIO_Port GPIOB
+#define PYRO_4_Pin GPIO_PIN_4
+#define PYRO_4_GPIO_Port GPIOB
+#define PYRO_4_SENSE_Pin GPIO_PIN_5
+#define PYRO_4_SENSE_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ README.md
@@ -0,0 +1,2 @@
+# navion-dev
+ 
