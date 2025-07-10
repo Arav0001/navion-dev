1c689ca19214733f637e4803b6c80f7c728b34bb
ASTR0N0M3R11 <arav.sonawane@gmail.com>
2025-07-10 02:30:48 -0400

    Beginnings of a Launch Server!

--- Code/CNTRL ESP32/src/main.cpp
+++ Code/CNTRL ESP32/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define AP_SSID "NAVION-FC"
+#define AP_PASSWORD "12345678"
+
+WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/html", R"rawliteral(
+    <h1>NAVION Launch Control</h1>
+    <button onclick="fetch('/launch')">Launch</button>
+  )rawliteral");
+}
+
+void handleLaunch() {
+  server.send(200, "text/plain", "Launch command sent.");
+  Serial.println("Launch command received.");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(115200);
+  	WiFi.softAP(AP_SSID, AP_PASSWORD);
+  	Serial.println("AP started at " + WiFi.softAPIP().toString());
+
+  	server.on("/", handleRoot);
+  	server.on("/launch", handleLaunch);
+  	server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	server.handleClient();
 }
