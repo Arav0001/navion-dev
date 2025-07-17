53e59eef32b6a198d7c419f37e71bebd36daf42f
ASTR0N0M3R11 <arav.sonawane@gmail.com>
2025-07-17 02:43:20 -0400

    ESP32 WebSockets

--- Code/CNTRL ESP32/data/server.js
+++ Code/CNTRL ESP32/data/server.js
@@ -1,6 +1,34 @@
-function sendCommand(cmd) {
-    fetch(`/${cmd}`)
-    .then(res => res.text())
-    .then(msg => alert(msg))
-    .catch(err => alert("Error: " + err));
+let socket = null;
+
+function initSocket() {
+	socket = new WebSocket(`ws://${location.host}/socket`);
+
+	socket.onopen = () => {
+		console.log("WebSocket connected");
+	};
+
+	socket.onmessage = (event) => {
+		alert("Message from NAVION: " + event.data);
+		console.log("Message from NAVION:", event.data);
+	};
+
+	socket.onclose = () => {
+		console.log("WebSocket closed. Reconnecting...");
+		setTimeout(initSocket, 3000);
+	};
+
+	socket.onerror = (err) => {
+		console.error("WebSocket error:", err);
+		socket.close();
+	};
 }
+
+function sendCommand(command) {
+	if (socket && socket.readyState === WebSocket.OPEN) {
+		socket.send(command);
+	} else {
+		console.warn("WebSocket not connected yet.");
+	}
+}
+
+window.onload = initSocket;
--- Code/CNTRL ESP32/src/main.cpp
+++ Code/CNTRL ESP32/src/main.cpp
@@ -10,8 +10,35 @@
 #define AP_PASSWORD "12345678"
 
 AsyncWebServer server(80);
+AsyncWebSocket webSocket("/socket");
 
-void handleLaunch(AsyncWebServerRequest *request);
+void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+	String msg = "";
+	
+	switch (type) {
+		case WS_EVT_CONNECT:
+			Serial.printf("WebSocket client connected: %u\n", client->id());
+			break;
+		case WS_EVT_DISCONNECT:
+			Serial.printf("WebSocket client disconnected: %u\n", client->id());
+			break;
+		case WS_EVT_DATA:
+			for (size_t i = 0; i < len; i++) msg += (char)data[i];
+
+			Serial.printf("[WS] Received from client %u: %s\n", client->id(), msg.c_str());
+
+			if (msg == "launch") {
+				Serial.println("[CMD] Launch command received");
+				client->text("Launch command acknowledged");
+			} else {
+				Serial.printf("[CMD] Unknown command: %s\n", msg.c_str());
+			}
+
+			break;
+		default:
+			break;
+	}
+}
 
 void setup() {
 	Serial.begin(115200);
@@ -26,16 +53,12 @@ void setup() {
 
 	server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html");
 
-	server.on("/launch", HTTP_GET, handleLaunch);
+	webSocket.onEvent(onWebSocketEvent);
+	server.addHandler(&webSocket);
 
   	server.begin();
 }
 
 void loop() {
 	// Nothing to do here
-}
-
-void handleLaunch(AsyncWebServerRequest *request) {
-	Serial.println("[CMD] Launch command received");
-	request->send(200, "text/plain", "Launch command sent.");
 }
