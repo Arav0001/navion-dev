d167632740c1169e9260baaf99ae0ce362a11d19
ASTR0N0M3R11 <arav.sonawane@gmail.com>
2025-08-08 02:15:15 -0400

    Simple PID code

--- Code/CNTRL/Core/Inc/control.h
+++ Code/CNTRL/Core/Inc/control.h
@@ -8,6 +8,24 @@
 #ifndef INC_CONTROL_H_
 #define INC_CONTROL_H_
 
+#include "main.h"
 
+typedef struct {
+	float Kp, Ki, Kd;
+
+	const float OUT_MAX;
+	const float OUT_MIN;
+
+	float integral;
+
+	float last_error;
+	float last_measure;
+	uint32_t last_ms;
+
+	float output;
+} PID;
+
+void PID_init(PID* pid);
+void PID_compute(PID* pid, float target, float current);
 
 #endif /* INC_CONTROL_H_ */
--- Code/CNTRL/Core/Src/control.c
+++ Code/CNTRL/Core/Src/control.c
@@ -5,4 +5,32 @@
  *      Author: aravs
  */
 
+#include "control.h"
 
+void PID_init(PID* pid) {
+	pid->last_ms = HAL_GetTick();
+}
+
+void PID_compute(PID* pid, float target, float current) {
+	float curr_ms = HAL_GetTick();
+
+	float dt = (curr_ms - pid->last_ms) / 1000.0f;
+	if (dt <= 0.0f) dt = 0.001f;
+
+	float error = target - current;
+
+	pid->integral += error * dt;
+
+	float derivative = (current - pid->last_measure) / dt;
+
+	float output =	pid->Kp * error +
+					pid->Ki * pid->integral +
+					pid->Kd * derivative;
+
+	if (output > pid->OUT_MAX) output = pid->OUT_MAX;
+	if (output < pid->OUT_MIN) output = pid->OUT_MIN;
+
+	pid->last_measure = current;
+	pid->last_ms = curr_ms;
+	pid->output = output;
+}
